455643958c7a17b9e7c6060246c1cc1f82d1afc7
Suzukisch-lang <zuzana.schiller@gmail.com>
2026-04-27 13:37:26 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-5xl font-bold text-black">Suzu vibuje!</h1>
+    </div>
+  );
+}
